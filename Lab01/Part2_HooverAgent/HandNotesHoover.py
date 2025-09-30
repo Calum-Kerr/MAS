e7db882a93ad77ba48f3d46d7d41633a455b0b83
Calum Kerr <calumxkerr@gmail.com>
2025-09-30 14:11:58 +0100

    from notes

---
+++ Lab01/Part2_HooverAgent/HandNotesHoover.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import asyncio, random
+
+location = ?
+grid = ?
+
+class Agent():
+    async def run(self):
+        global location, grid
+        while True:
+            ?
+            await asyncio.sleep(1)
+
+async def environment():
+    while True:
+        ?
+        await asyncio.sleep(1)
+
+async def main():
+    agent = Agent()
+    await asyncio.gather(environment(),agent.run())
+
+if __name__ == "__main__":
+    asyncio.run(main())
