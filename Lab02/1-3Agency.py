e93b45421153b5f50534c9b3717a6a104bf31185
Calum Kerr <93798847+Calum-Kerr@users.noreply.github.com>
2025-10-02 14:14:27 +0000

    combinations

---
+++ Lab02/1-3Agency.py
@@ -0,0 +1,25 @@
+
+
+'''
+
+if its a 5 x 5 board then these are viable winning combos:
+
+    0|1|2|3|4
+    5|6|7|8|9
+    1|2|3|4|5
+    6|7|8|9|2
+    2|2|2|2|2
+
+    3 per row
+    3 per coloum
+    =
+    9
+    +
+    1,2,3,2,1
+    +
+    1,2,3,2,1
+    =
+    36 possible ways to win?
+
+
+'''
