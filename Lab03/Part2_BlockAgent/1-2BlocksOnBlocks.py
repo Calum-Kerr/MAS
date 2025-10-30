079d01556c35f05de2a359ac20f36b13e4a4b43d
Calum Kerr <93798847+Calum-Kerr@users.noreply.github.com>
2025-10-30 14:50:18 +0000

    made block move onto block but doesnt work that well :(

---
+++ Lab03/Part2_BlockAgent/1-2BlocksOnBlocks.py
@@ -0,0 +1,23 @@
+#!usr/bin/env python3 
+
+import asyncio,random
+
+block_state={'a':'table','b':'table','c':'table'}
+
+class Agent():
+    async def run(self):
+        global block_state
+        while True:
+            all_blocks=['a','b','c']
+            block_to_move=random.choice(all_blocks)
+            destination=random.choice([b for b in all_blocks if b !=block_to_move])
+            block_state[block_to_move]=destination
+            print(f"moved: {block_to_move} onto {destination}")
+            await asyncio.sleep(1)
+
+async def main():
+    agent=Agent()
+    await asyncio.gather(agent.run())
+
+if __name__=="__main__":
+    asyncio.run(main())
