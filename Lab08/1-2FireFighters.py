4d7cee79462025d6dadc0dbfd0550ff10319be41
Calum Kerr <calumxkerr@gmail.com>
2025-12-01 22:30:18 +0000

    troglodyte's rocking a semi... fire!

--- Lab08/1-2FireFighters.py
+++ Lab08/1-2FireFighters.py
@@ -11,4 +11,10 @@ class Environment():
         self.width=width
         self.height=height
         self.grid=[[EMPTY for _ in range(width)] for _ in range(height)]
-    
+    def display(self):
+        for row in self.grid:
+            print(''.join(row))
+        print()
+    def start_fire(self,x,y):
+        if 0<=x<self.width and 0<=y<self.height:
+            self.grid[y][x]=FIRE
