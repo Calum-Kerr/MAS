b57a6b38046f097b8f5d736cf617b0a4e08b98a8
Calum Kerr <calumxkerr@gmail.com>
2025-12-01 22:36:31 +0000

    spread those ******

--- Lab08/1-2FireFighters.py
+++ Lab08/1-2FireFighters.py
@@ -18,11 +18,28 @@ class Environment():
     def start_fire(self,x,y):
         if 0<=x<self.width and 0<=y<self.height:
             self.grid[y][x]=FIRE
+    def spread_fires(self):
+        new_fires=[]
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.grid[y][x]==FIRE:
+                    neighbours=[(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+                    for nx,ny in neighbours:
+                        if 0<=nx<self.width and 0<=ny<self.height:
+                            if self.grid[ny][nx]==EMPTY:
+                                if random.random()<0.5:
+                                    new_fires.append((nx,ny))
+        for x,y in new_fires:
+            self.grid[y][x]=FIRE
+        return len(new_fires)
 
 def main():
     env=Environment(10,10)
     env.start_fire(5,5)
-    env.display()
+    for turn in range(5):
+        print(f"Turn {turn}")
+        env.display()
+        env.spread_fires()
 
 if __name__=="__main__":
     main()
