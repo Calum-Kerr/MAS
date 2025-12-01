96345bda2f4bd133ee932f2daeea1f3ee23572c4
Calum Kerr <calumxkerr@gmail.com>
2025-12-01 22:10:26 +0000

    task class

--- Lab08/1-1ContractNet.py
+++ Lab08/1-1ContractNet.py
@@ -4,6 +4,12 @@ import uuid,random
 
 ALL_SKILLS=['cooking hello fresh meals.. lol','cleaning the houuse (total dont use a robot for it)', 'driving (i was sideswiped by a police van)', 'writing (still shit at english though)']
 
+class Task():
+    def __init__(self,skill_required,duration=1):
+        self.id=uuid.uuid4()
+        self.skill_required=skill_required
+        self.duration=duration
+        self.status='open'
 class Agent():
     def __init__(self,name=None):
         self.uid=uuid.uuid4()
@@ -15,6 +21,8 @@ def main():
     agents=[Agent(f"agent{str(i)}") for i in range(10)]
     for agent in agents:
         print(f"{agent.name} can {agent.capabilities}")
+    task=Task('cooking hello fresh meals.. lol')
+    print(f"task {task.id} requires {task.skill_required}")
 
 if __name__=="__main__":
     main()
