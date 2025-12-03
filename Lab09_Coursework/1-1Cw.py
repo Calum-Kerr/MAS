01ffdd71c233c97eea34d38dfc8cc1383fdd3813
Calum Kerr <calumxkerr@gmail.com>
2025-12-03 17:55:18 +0000

    a base

---
+++ Lab09_Coursework/1-1Cw.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import random,os
+
+EMPTY='.'
+CASUALTY='C'
+SEARCHED='~'
+
+class Environment():
+    def __inti__(self,width=20,height=10):
+        self.width=width
+        self.height=height
+        self.grid=[[EMPTY for _ in range(width)]for _ in range(height)]
+    def display(self):
+        for row in self.grid:
+            print(''.join(row))
+    def clear_screen(self):
+        os.system('cls'if os.name=='nt'else'clear')
+
+def main():
+    env=Environment(20,10)
+    env.display()
+
+if __name__=="__main__":
+    main()
